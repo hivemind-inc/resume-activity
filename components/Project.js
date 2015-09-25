676d9acf42d56b3da344fb0481ca0cbdfe767e3f
pivotal <pivotal@speer.den.pivotal.io>
2015-09-25 13:59:52 -0600

    remove log

--- components/Project.js
+++ components/Project.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 var Project = React.createClass({
   render: function() {
-    console.log('this.props', this.props);
     return (
       <p>{this.props.project_name}</p>
     )
